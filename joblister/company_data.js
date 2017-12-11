364a31ceca3c705c6ec0372febe7239578fd92d6
Varun Bhat <varunbhat.kn@gmail.com>
2017-12-10 19:28:38 -0700

    timed requests

--- joblister/company_data.js
+++ joblister/company_data.js
@@ -1,26 +1,26 @@
 workday_links = {
     nvidia: {
-        url: 'https://nvidia.wd5.myworkdayjobs.com/NVIDIAExternalCareerSite',
+        url: 'https://nvidia.wd5.myworkdayjobs.com/NVIDIAExternalCareerSite/login',
         facet_filter: {'locations': ['US,']}
     },
     infinera: {
-        url: 'https://infinera.wd1.myworkdayjobs.com/Infinera_Careers',
+        url: 'https://infinera.wd1.myworkdayjobs.com/Infinera_Careers/login',
         facet_filter: {'locations': ['Sunnyvale', 'Allentown', 'Annapolis Junction']}
     },
-    intel: 'https://intel.wd1.myworkdayjobs.com/en-US/External/jobs',
+    intel: {url: 'https://intel.wd1.myworkdayjobs.com/en-US/External/login'},
     broadcom: {
-        url: 'https://avagotech.wd1.myworkdayjobs.com/External_Career',
+        url: 'https://avagotech.wd1.myworkdayjobs.com/External_Career/login',
         facet_filter: {'locations': ['USA']}
     },
-    arrow: 'https://arrow.wd1.myworkdayjobs.com/en-US/AC/',
-    cadence: 'https://cadence.wd1.myworkdayjobs.com/External_Careers',
-    dell: 'https://dell.wd1.myworkdayjobs.com/en-US/External/jobs',
-    hp: 'https://hp.wd5.myworkdayjobs.com/ExternalCareerSite/',
-    hpe: 'https://hpe.wd5.myworkdayjobs.com/Jobsathpe',
-    bose: 'https://boseallaboutme.wd1.myworkdayjobs.com/Bose_Careers/jobs',
-    nxp: 'https://nxp.wd3.myworkdayjobs.com/careers',
-    vmware: 'https://vmware.wd1.myworkdayjobs.com/en-US/VMware',
-    equinix: 'https://equinix.wd1.myworkdayjobs.com/External'
+    arrow: {url: 'https://arrow.wd1.myworkdayjobs.com/en-US/AC/login'},
+    cadence: {url: 'https://cadence.wd1.myworkdayjobs.com/External_Careers/login'},
+    dell: {url: 'https://dell.wd1.myworkdayjobs.com/en-US/External/login'},
+    hp: {url: 'https://hp.wd5.myworkdayjobs.com/ExternalCareerSite/login'},
+    hpe: {url: 'https://hpe.wd5.myworkdayjobs.com/Jobsathpe/login'},
+    bose: {url: 'https://boseallaboutme.wd1.myworkdayjobs.com/Bose_Careers/login'},
+    nxp: {url: 'https://nxp.wd3.myworkdayjobs.com/careers/login'},
+    vmware: {url: 'https://vmware.wd1.myworkdayjobs.com/en-US/VMware/login'},
+    equinix: {url: 'https://equinix.wd1.myworkdayjobs.com/External/login'}
 };
 
 
--- joblister/jobphantomize.js
+++ joblister/jobphantomize.js
@@ -1,139 +1,124 @@
-var page = require('webpage').create();
 var fs = require('fs');
 
-company = require('./company_data.js').broadcom;
+company = require('./company_data.js');
 
-page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/604.3.5 (KHTML, ' +
-    'like Gecko) Version/11.0.1 Safari/604.3.2';
+var retimize_interval = 0;
 
-page.viewportSize = {
-    width: 1440, height: 900
-};
+Object.keys(company).forEach(function (company_name) {
+    var page = require('webpage').create();
 
-page.onConsoleMessage = function (msg) {
-    console.log('Page Log: ' + msg);
-    // fs.write('result.json', msg, 'w');
-};
+    page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/604.3.5 (KHTML, ' +
+        'like Gecko) Version/11.0.1 Safari/604.3.2';
 
-page.onUrlChanged = function (targetUrl) {
-    // console.log('New URL: ' + targetUrl);
-};
-
-page.onResourceRequested = function (req, networkRequest) {
-    // console.log('requested: ' + JSON.stringify(req.url, undefined, 4));
-};
-
-page.onResourceReceived = function (res) {
-    // console.log('Response (#' + res.id + ', stage "' + res.stage + '"): ' + JSON.stringify(res, null, 2));
-    // console.log('received: ' + JSON.stringify(res.url, undefined, 4));
-    // if (res.contentType === 'application/json' && res.stage === 'end') {
-    // console.log('received: ' + JSON.stringify(res.url, undefined, 4));
-    // var file_name = res.url.split('?')[0].split('/').splice(-1)[0];
-    // fs.write('results/' + file_name + '.json', res.body, 'w');
-    // }
-};
-
-function setFacets(company_name, filter) {
-    return page.evaluate(function (company_name, filter) {
-
-        function check_filter_condition(value, filter) {
-            // console.log(JSON.stringify(filter), value);
-            var res = false;
-            if (filter) {
-                filter.forEach(function (fc) {
-                    // console.log(fc, value, value.indexOf(fc) !== -1);
-                    res = res || (value.indexOf(fc) !== -1);
-                });
-            }
-            return res;
-        }
-
-        var results = filterSearch(facet_filter, document);
-
-        // console.log(JSON.stringify(results, null, 2));
-        var timeout_intervals = 0;
-        Object.keys(results).forEach(function (facet) {
-            results[facet].forEach(function (value) {
-                if (check_filter_condition(value.name, filter[facet])) {
-                    setTimeout(function () {
-                        var facet_id = value.id.split('-').splice(-1)[0];
-                        // console.log(facet_id);
-                        var element = document.getElementById('wd-FacetValue-CheckBox-' + facet_id + '-input');
-                        element.click();
-                    }, timeout_intervals);
-                    timeout_intervals += 100;
-                }
-            })
-        });
-    }, company_name, filter);
-
-}
-
-page.open(company.url + '/login', function () {
-    page.evaluate(function () {
-        vb_hacks = {};
-        (function (open) {
-            XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
-                this.addEventListener("readystatechange", function () {
-                    if (XMLHttpRequest.DONE === this.readyState && this.getResponseHeader('Content-Type') === 'application/json') {
-                        // console.log("AJAX response: " + url);
-                        vb_hacks['data'] = vb_hacks['data'] || [];
-                        vb_hacks['data'].push({url: url, data: JSON.parse(this.responseText)});
-                    }
-                }, false);
-                open.apply(this, arguments);
-            };
-        })(XMLHttpRequest.prototype.open);
-    });
-
-    login_fill = function () {
-        page.evaluate(function () {
-            var username = document.evaluate(
-                '//*[@data-automation-id="auth_container"]//div[@data-automation-id="userName"]/input', document, null, 0, null)
-                .iterateNext();
-            var password = document.evaluate(
-                '//*[@data-automation-id="auth_container"]//div[@data-automation-id="password"]/input', document, null, 0, null)
-                .iterateNext();
-
-            var signin = document.evaluate(
-                '//*[@data-automation-id="auth_container"]//div[@data-automation-id="click_filter"]', document, null, 0, null)
-                .iterateNext();
-
-
-
-            // console.log(username.value);
-            // console.log(password.value);
-            signin.click();
-        });
+    page.viewportSize = {
+        width: 1440, height: 900
     };
 
+    page.onConsoleMessage = function (msg) {
+        console.log('Page Log: ' + msg);
+        // fs.write('result.json', msg, 'w');
+    };
+
+    page.onUrlChanged = function (targetUrl) {
+        console.log('New URL: ' + targetUrl);
+    };
+
+    page.onResourceRequested = function (req, networkRequest) {
+        // console.log('requested: ' + JSON.stringify(req.url, undefined, 4));
+    };
+
+    page.onResourceReceived = function (res) {
+        // console.log('Response (#' + res.id + ', stage "' + res.stage + '"): ' + JSON.stringify(res, null, 2));
+        // console.log('received: ' + JSON.stringify(res.url, undefined, 4));
+        // if (res.contentType === 'application/json' && res.stage === 'end') {
+        // console.log('received: ' + JSON.stringify(res.url, undefined, 4));
+        // var file_name = res.url.split('?')[0].split('/').splice(-1)[0];
+        // fs.write('results/' + file_name + '.json', res.body, 'w');
+        // }
+    };
+
+    page.evaluate(function () {
+        // document.head.append = document.head.appendChild;
+    });
 
     setTimeout(function () {
-        login_fill();
-    }, 3000);
+        page.open(company[company_name].url, function () {
+            page.evaluate(function () {
+                vb_hacks = {};
 
-    setTimeout(function () {
-        page.evaluate(function () {
-            var home_button = document.evaluate(
-                '//*[@id="workdayApplicationHeader"]//span[@class="workdayLogo"]', document, null, 0, null)
-                .iterateNext();
-            // console.log(home_button);
-            home_button.click();
+                (function (open) {
+                    XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
+                        this.addEventListener("readystatechange", function () {
+                            if (XMLHttpRequest.DONE === this.readyState && this.getResponseHeader('Content-Type') === 'application/json') {
+                                // console.log("AJAX response: " + url);
+                                vb_hacks['data'] = vb_hacks['data'] || [];
+                                vb_hacks['data'].push({url: url, data: JSON.parse(this.responseText)});
+                            }
+                        }, false);
+                        open.apply(this, arguments);
+                    };
+                })(XMLHttpRequest.prototype.open);
+            });
+
+            login_fill = function () {
+                page.evaluate(function () {
+                    var username = document.evaluate(
+                        '//*[@data-automation-id="auth_container"]//div[@data-automation-id="userName"]/input', document, null, 0, null)
+                        .iterateNext();
+                    var password = document.evaluate(
+                        '//*[@data-automation-id="auth_container"]//div[@data-automation-id="password"]/input', document, null, 0, null)
+                        .iterateNext();
+
+                    var signin = document.evaluate(
+                        '//*[@data-automation-id="auth_container"]//div[@data-automation-id="click_filter"]', document, null, 0, null)
+                        .iterateNext();
+
+
+                    // console.log(username, password, signin);
+                    // console.log(username.value);
+                    // console.log(password.value);
+                    signin.click();
+                });
+            };
+
+            setTimeout(function () {
+                page.render('results/' + company_name + '_login.png');
+            }, 4000);
+
+            setTimeout(function () {
+                login_fill();
+            }, 4000);
+
+            setTimeout(function () {
+                page.render('results/' + company_name + '_userhome.png');
+            }, 5900);
+
+            setTimeout(function () {
+                page.evaluate(function () {
+                    var home_button = document.evaluate(
+                        '//*[@id="workdayApplicationHeader"]//span[@class="workdayLogo"]', document, null, 0, null)
+                        .iterateNext();
+                    // console.log(home_button);
+                    home_button.click();
+                });
+            }, 6000);
+
+            setTimeout(function () {
+                var data = page.evaluate(function () {
+                    return JSON.stringify(vb_hacks);
+                });
+
+                fs.write('results/' + company_name + '_home.json', data, 'w');
+            }, 9000);
+
+            setTimeout(function () {
+                page.render('results/' + company_name + '.png');
+            }, 9000);
+            // setInterval(phantom.exit, 10000);
         });
-    }, 4000);
-
-    setTimeout(function () {
-        var data = page.evaluate(function () {
-            return JSON.stringify(vb_hacks);
-        });
-
-        fs.write('results/results.json', data, 'w');
-    }, 10000);
-
-    setTimeout(function () {
-        page.render('broadcom_enc.png');
-        phantom.exit();
-    }, 10000);
-
-    // setInterval(phantom.exit, 10000);
+    }, retimize_interval);
+    retimize_interval += 9100;
+    // phantom.exit();
 });
+
+setTimeout(phantom.exit, retimize_interval);
